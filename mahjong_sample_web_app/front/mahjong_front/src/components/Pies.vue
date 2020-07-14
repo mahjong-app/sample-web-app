ade1ad77ef993b27355d5f57b12bd4cfac8f6422
Manabu TERADA <terada@cmscom.jp>
2020-07-14 22:21:22 +0900

    modify package something

--- mahjong_sample_web_app/front/mahjong_front/src/components/Pies.vue
+++ mahjong_sample_web_app/front/mahjong_front/src/components/Pies.vue
@@ -1,60 +1,35 @@
 <template>
   <div id="main" class="row">
-    <form action="/calc" method="GET">
-      <!-- {{var tiles=[1, 2, 3]}} -->
-      <div class="tile row" v-for="i in [1, 2, 3]">
-        <!-- {{var tile="i"}} -->
-        <!-- <div class="tile row"> -->
-        <img src="static/img/pi/1m.png" alt="&#x1F007;" class="rounded tile-item" width="25" />
-        <button
-          type="radio"
-          class="btn btn-secondary btn-sm tile-item"
-          data-toggle="button"
-          aria-pressed="false"
-          autocomplete="off"
-          name="tile-1-agari"
-          value="1"
-        >上牌</button>
-        <div class="btn-group btn-group-toggle tile-item" data-toggle="buttons">
-          <label class="btn btn-secondary btn-sm">
-            <input type="radio" name="tile-1-naki" id="tile-1-naki-1" value="1" autocomplete="off" />明
-          </label>
-          <label class="btn btn-secondary btn-sm">
-            <input type="radio" name="tile-1-naki" id="tile-1-naki-2" value="2" autocomplete="off" />明
-          </label>
-          <label class="btn btn-secondary btn-sm">
-            <input type="radio" name="tile-1-naki" id="tile-1-naki-3" value="3" autocomplete="off" />明
-          </label>
-          <label class="btn btn-secondary btn-sm">
-            <input type="radio" name="tile-1-naki" id="tile-1-naki-4" value="4" autocomplete="off" />明
-          </label>
-          <label class="btn btn-secondary btn-sm">
-            <input type="radio" name="tile-1-naki" id="tile-1-naki-5" value="5" autocomplete="off" />槓
-          </label>
-        </div>
-        <button
-          type="radio"
-          class="btn btn-secondary btn-sm tile-item"
-          data-toggle="button"
-          aria-pressed="false"
-          autocomplete="off"
-          name="tile-1-dora"
-          value="1"
-        >ドラ</button>
-        <div class="btn-group btn-group-toggle tile-item" data-toggle="buttons">
-          <label class="btn btn-secondary btn-sm">
-            <input type="checkbox" autocomplete="off" name="aka-1" value="1" />赤
-          </label>
-        </div>
-      </div>
-    </form>
+
+    <!-- <form action="/calc" method="GET"> -->
+    <!-- {{var tiles=[1, 2, 3]}} -->
+    <div class="tile row" v-for="pi in pies" id="pi-buttons">
+      <Pi />
+    </div>
+    <!-- </form> -->
   </div>
 </template>
 <script>
+import store from "../store";
+import Pi from "./Pi.vue";
+import { mapState } from "vuex";
 export default {
-  name: "Pies"
+  name: "Pies",
+  components: {
+    Pi
+  },
+  computed: mapState({
+    pies: state => state.pies,
+    isAgariActive: (state, pi) => state.isAgariActive,
+    // ここで、動的に状態を取ってきたいので関数化をする。
+    isDraActive: state => state.isDraActive,
+    isAkaActive: state => state.isAkaActive
+  }),
+  methods: {
+  }
 };
 </script>
 
 <style scoped lang="scss">
+
 </style>
--- mahjong_sample_web_app/front/mahjong_front/src/store/index.js
+++ mahjong_sample_web_app/front/mahjong_front/src/store/index.js
@@ -1,15 +1,74 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+// function toggleActive(state, attr) {
+//   attrObj = state[attr];
+//   console.log(attrObj);
+//   if (attrObj) {
+//     attrObj = false;
+//   } else {
+//     attrObj = true;
+//   }
+//   console.log(attrObj);
+// }
 
 export default new Vuex.Store({
   state: {
+    pies: [
+      { id: "pi1", name: "2m", agari: false, naki: 1, dora: false, aka: false },
+      { id: "pi2", name: "3m", agari: false, naki: 1, dora: false, aka: false }
+    ], // [{id: "pi1", name: "2m", agari: false, naki: 1, dora: false, aka: false}]
+    // agari: null, // id of pi
+    // nakipies: { naki1: [], naki2: [], naki3: [], naki4: [] },
+    ba: "ton",
+    kaze: "ton",
+    reach: false,
+    ron: false,
+    tsumo: false,
+    isAgariActive: {},
+    isDraActive: false,
+    isAkaActive: false
   },
   mutations: {
+    updateAgari(state, pi) {
+      // toggleActive(state, state.isAgariActive);
+      // この部分のforを作る。
+      // プロパティへの保存が伝播するのか？
+      for (let p of state.pies) {
+        if (pi === p.index) {
+          p.agari = true
+        } else if (p.agari) {
+          p.agari = false
+        }
+      }
+      // state.pies[pi]["agari"] = true;
+
+      // state.agari = pi;
+
+      if (state.isAgariActive) {
+        state.isAgariActive = false;
+      } else {
+        state.isAgariActive = true;
+      }
+    },
+    updateNaki(state, pi, num) {},
+    updateDra(state, pi) {
+      if (state.isDraActive) {
+        state.isDraActive = false;
+      } else {
+        state.isDraActive = true;
+      }
+    },
+    updateAka(state, pi) {
+      if (state.isAkaActive) {
+        state.isAkaActive = false;
+      } else {
+        state.isAkaActive = true;
+      }
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {}
+});
